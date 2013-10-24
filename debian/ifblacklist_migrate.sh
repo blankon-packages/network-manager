3501172ce0af1eb47d954edd9d53e8e566517451
Mahyuddin Idram Ahmad <dotovr@di.blankon.in>
2013-10-24 21:31:15 +0700

    New upstream release

--- debian/ifblacklist_migrate.sh
+++ debian/ifblacklist_migrate.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+#set -x
+
 # (C) 2007 Canonical Ltd.
 # Author: Alexander Sack <asac@jwsdot.com>
+# Author: Mathieu Trudel-Lapierre <mathieu.trudel-lapierre@canonical.com>
 # License: GNU General Public License, version 2 or any later version
 
 if test x$NIF_FILE = x; then
@@ -19,35 +22,11 @@ echo Auto interfaces found: $auto_ifs
 
 # iterate over all auto interfaces
 for i in $auto_ifs; do
-  IFS_old=$IFS; IFS=""
 
-  NIF_FILE_content=$(cat $NIF_FILE | \
-      sed -e 's/^[ \t]*auto.*$//' | \
-      sed -e 's/^[ \t]*allow-.*$//' | \
-      sed -e 's/^[ \t]*#.*$//' | grep -v ^$)
-
-  # '--' is inserted by grep -A1 if there are multiple iface blocks
-  lines=$(echo $NIF_FILE_content | grep -A1 "^iface.*$i.*dhcp" | grep -v '\--')
-  IFS="
-"
-
-  # if there is no iface line for that interface, we would still get a line
-  # count of 1 ... so use word_count 0 below to exclude ifaces that have no
-  # configuration at all.
-  word_count=$(echo $lines | wc -w)
-  line_count=0
-  for line in $lines; do
-      nulled_line=$(echo "$line" | sed -e 's/[# ]//' | grep -v ^iface)
-      if test x$nulled_line != x; then
-	  line_count=$(expr $line_count + 1)
-      fi
-  done
-
-  if test $line_count -eq 0 -a $word_count -gt 0; then
+  if grep -q "^[[:space:]]*iface[[:space:]]\+$i[[:space:]].*\(dhcp\|auto\)" $NIF_FILE; then
      ifaces_to_disable="$ifaces_to_disable $i"
      echo iface to disable = $i
   fi
-  IFS=$IFS_old
 done
 
 backup_suffix=0
@@ -59,7 +38,7 @@ if [ -n "$ifaces_to_disable" ]; then
     cp $NIF_FILE "$NIF_FILE.bak-${backup_suffix}"
     for i in $ifaces_to_disable; do
 	echo -n "Disabling interface: $i ... "
-	sed -i -e "s/^\([ \t]*iface.*[ \t]$i[ \t].*\)$/#NetworkManager#\1/" $NIF_FILE
+	sed -i -e"/^[[:space:]]*auto[[:space:]]\+$i/,/^[[:space:]]*\(auto\|mapping\|source\|allow-\)/ { /^[[:space:]]*\(auto\|iface\)[[:space:]]\+$i/ s/^/#NetworkManager#/; /^[[:space:]]*\(auto\|iface\|mapping\|source\|allow-\)/ ! s/^\([^#]\)/#NetworkManager#\1/; }" $NIF_FILE
 	echo done.
     done
 fi
